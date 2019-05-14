d3f6826144284bc21d82a3a5aefbee512340d1d4
Clifford Gurney <clifford.gurney@gmail.com>
2019-05-14 21:20:17 +1000

    wip

---
+++ client/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+      <v-text-field flex prepend-icon="search" placeholder="Search" single-line-hide-details></v-text-field>
+
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items></v-toolbar-items>
+    </v-toolbar>
+
+
+
+    <v-content>
+      <Home/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Home from './components/Home'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
